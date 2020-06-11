e0d3a0b6d98ef2879110847e92f68a945e11c974
marinamiyazawa <mi.marina1030@gmail.com>
2020-06-11 18:27:36 +0900

    favorite

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,10 @@
 class PostsController < ApplicationController
+
 	def new
 		@post = Post.new
 		@post.post_images.build
 	end
+
 	def create
 		@post = Post.new(post_params)
 		@post.user_id = current_user.id
@@ -10,18 +12,25 @@ class PostsController < ApplicationController
 		@post.save
 		redirect_to post_path(@post)
 	end
+
 	def index
 		@post = Post.all
 		@user = current_user
 	end
+
 	def show
 		@post = Post.find(params[:id])
 		@user = @post.user
+	end
 
-	end
 	def edit
+		@post = Post.find(params[:id])
 	end
+
 	def update
+		@post = Post.find(params[:id])
+		@post.update(post_params)
+		redirect_to @post
 	end
 
 	private
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,7 @@ class UsersController < ApplicationController
 
 	def show
 		@user = User.find(params[:id])
+		@posts = @user.posts
 		@currentUserEntry = Entry.where(user_id: current_user.id)
 		@userEntry = Entry.where(user_id: @user.id)
 		if @user.id == current_user.id
