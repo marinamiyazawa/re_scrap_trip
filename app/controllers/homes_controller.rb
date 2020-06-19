8faca41e4d261d372c8513e52905773288d13fb9
vagrant <vagrant@localhost.localdomain>
2020-06-19 07:00:39 +0000

    [update]header

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,5 +1,7 @@
 class HomesController < ApplicationController
 	def top
+		@posts = Post.published.order("created_at DESC").limit(4)
+		render layout: false #application.html.erb
 	end
 	def about
 		render layout: false #application.html.erb
