1a6a681140d1e12a5510d8aff1613045c0b1f81e
marinamiyazawa <mi.marina1030@gmail.com>
2020-06-25 22:50:14 +0900

    [update]

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,10 +1,10 @@
 class HomesController < ApplicationController
 	def top
-		@posts = Post.published.order("created_at DESC").limit(4)
+		@posts = Post.published.order(created_at: :desc).limit(4)
 		render layout: false #application.html.erb
 	end
 	def about
-		@posts = Post.published.order("created_at DESC").limit(3)
+		@posts = Post.published.order(created_at: :desc).limit(3)
 		render layout: false #application.html.erb
 	end
 end
