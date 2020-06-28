c067250abf3dde2ce9cd3cfd743077c9d418fd6a
marinamiyazawa <mi.marina1030@gmail.com>
2020-06-28 18:42:30 +0900

    [update]room

--- app/models/room.rb
+++ app/models/room.rb
@@ -3,4 +3,13 @@ class Room < ApplicationRecord
 	has_many :entries, dependent: :destroy
 	has_many :users, through: :entries
 	attachment :image
+
+	def partner(user)
+		#自分以外のエントリーを返す
+		if user == entries.first.user
+			entries.second.user
+		else
+			entries.first.user
+		end
+	end
 end
