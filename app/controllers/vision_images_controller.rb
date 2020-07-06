8465eb9cd4f39b8b1135873d0f170a58e4b13a13
marinamiyazawa <mi.marina1030@gmail.com>
2020-07-06 21:31:43 +0900

    [update]validation

--- app/controllers/vision_images_controller.rb
+++ app/controllers/vision_images_controller.rb
@@ -4,33 +4,36 @@ class VisionImagesController < ApplicationController
 	end
 	def create
 		@vision_image = VisionImage.new(vision_images_params)
-		@vision_image.save
-		tags = Vision.get_image_data(@vision_image.image,"label")
-		tags.each do |tag|
-			@vision_image.tags.create(name: tag[0],score: tag[1])
-		end
-		#landmarks.each do |landmark|
-			#a = @vision_image.landmarks.create(name: landmark[0],score: landmark[1],locations: landmark[2])
-			#latlng = a.locations.scan(/[+\-]?\d{1,3}.\d{1,14}/)
-			#lat = latlng[0]
-			#lng = latlng[1]
-			#a.latitude = lat
-			#a.longitude = lng
-			#a.save
-		#end
-		landmarks = Vision.get_image_data(@vision_image.image,"landmark").flatten
-		#landmarkがあるか確認
-		if landmarks.blank?
+		if @vision_image.save
+			tags = Vision.get_image_data(@vision_image.image,"label")
+			tags.each do |tag|
+				@vision_image.tags.create(name: tag[0],score: tag[1])
+			end
+			#landmarks.each do |landmark|
+				#a = @vision_image.landmarks.create(name: landmark[0],score: landmark[1],locations: landmark[2])
+				#latlng = a.locations.scan(/[+\-]?\d{1,3}.\d{1,14}/)
+				#lat = latlng[0]
+				#lng = latlng[1]
+				#a.latitude = lat
+				#a.longitude = lng
+				#a.save
+			#end
+			landmarks = Vision.get_image_data(@vision_image.image,"landmark").flatten
+			#landmarkがあるか確認
+			if landmarks.blank?
+			else
+				@vision_image.landmarks.create(
+				name: landmarks[0],
+				score: landmarks[1],
+				locations: landmarks[2],
+				latitude: landmarks[2]["latLng"]["latitude"],
+				longitude: landmarks[2]["latLng"]["longitude"]
+			)
+			end
+			redirect_to vision_image_path(@vision_image)
 		else
-			@vision_image.landmarks.create(
-			name: landmarks[0],
-			score: landmarks[1],
-			locations: landmarks[2],
-			latitude: landmarks[2]["latLng"]["latitude"],
-			longitude: landmarks[2]["latLng"]["longitude"]
-		)
+			render :new
 		end
-		redirect_to vision_image_path(@vision_image)
 	end
 	def show
 		@vision_image = VisionImage.find(params[:id])
